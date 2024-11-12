81d555aa5e39c0a2ca562e17fae78b43a269a4b7
Reginaldo Spricigo <reginaldo.spricigo@weinnovate.com.br>
2024-11-11 23:08:08 -0300

    [Feature] Load project cards dinamicaly

--- js/app.js
+++ js/app.js
@@ -1,5 +1,20 @@
 const contentDiv = document.getElementById("content");
 
+// Lista de projetos para exibir nos cards
+const projects = [
+  {
+    image: 'url-da-imagem-1.jpg',
+    title: 'Projeto 1',
+    description: 'Uma breve descrição do projeto 1.'
+  },
+  {
+    image: 'url-da-imagem-2.jpg',
+    title: 'Projeto 2',
+    description: 'Uma breve descrição do projeto 2.'
+  },
+  // Adicione outros projetos conforme necessário
+];
+
 async function loadPage(page){
     try{
 
@@ -11,6 +26,12 @@ async function loadPage(page){
 
         const html = await response.text();
         contentDiv.innerHTML = html;
+        if(page === 'projects'){
+            loadCards();
+        }else{
+            const cardsContainer = document.getElementById('cards');
+            cardsContainer.innerHTML = '';
+        }
     }
     catch(error){
         console.error(error);
@@ -21,6 +42,34 @@ async function loadPage(page){
 function navigate(page) {
     window.location.hash = page;
     loadPage(page);
+}
+
+async function loadCards(){
+    const cardsContainer = document.getElementById('cards');
+
+    try{
+        const response = await fetch('components/card.html');
+        if(!response.ok){
+            throw new Error(`Failed to load cards: ${response.status}`);
+        }
+        const html = await response.text();
+
+        projects.forEach(project => {
+            const cardTemplate = document.createElement('div');
+            cardTemplate.innerHTML = html.trim();
+
+            const card = cardTemplate.firstElementChild;            
+            card.querySelector('.project-card-image').src = project.image;
+            card.querySelector('.project-card-image').alt = project.title
+            card.querySelector('.project-card-title').textContent = project.title;
+            card.querySelector('.project-card-description').textContent = project.description;
+            cardsContainer.appendChild(card);
+        });
+
+       // document.getElementById('cards').innerHTML = html;
+    } catch(error){
+        console.error('There was a problem loading the cards:', error);
+    }
 }
 
 async function loadComponent(componentPath, elementId) {
